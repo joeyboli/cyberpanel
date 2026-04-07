3699722010d6ee3d6d7cc5ad1c65cab658759824
Joey 😎 <43940776+joeyboli@users.noreply.github.com>
2026-04-07 16:59:59 +0000

    d

--- plogical/phpmyadminsignin.php
+++ plogical/phpmyadminsignin.php
@@ -14,13 +14,16 @@ try {
     define('PMA_SIGNON_SESSIONNAME', 'SignonSession');
     define('PMA_DISABLE_SSL_PEER_VALIDATION', TRUE);
 
-    if (isset($_POST['password']) && isset($_POST['username'])) {
+    // Merge GET and POST to handle both ways of passing token/username
+    $request = array_merge($_GET, $_POST);
+
+    if (isset($request['password']) && isset($request['username'])) {
 
         session_name(PMA_SIGNON_SESSIONNAME);
         @session_start();
 
-        $username = htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8');
-        $password = $_POST['password'];
+        $username = htmlspecialchars($request['username'], ENT_QUOTES, 'UTF-8');
+        $password = $request['password'];
 
         $_SESSION['PMA_single_signon_user'] = $username;
         $_SESSION['PMA_single_signon_password'] = $password;
@@ -31,27 +34,24 @@ try {
         header('Location: /phpmyadmin/index.php?server=' . PMA_SIGNON_INDEX);
         exit;
         
-    } else if (isset($_POST['token'])) {
+    } else if (isset($request['token'])) {
 
         ### Get credentials using the token
 
-        $token = htmlspecialchars($_POST['token'], ENT_QUOTES, 'UTF-8');
-        $username = htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8');
+        $token = htmlspecialchars($request['token'], ENT_QUOTES, 'UTF-8');
+        $username = htmlspecialchars($request['username'], ENT_QUOTES, 'UTF-8');
 
-        //$url = "/dataBases/fetchDetailsPHPMYAdmin?token=" . $token . '&username=' . $username;
         $url = "/dataBases/fetchDetailsPHPMYAdmin";
 
-        //         header('Location: ' . $url);
-
         // Redirect with POST data
-
         echo '<form id="redirectForm" action="' . $url . '" method="post">';
         echo '<input type="hidden"  value="' . $token . '" name="token">';
         echo '<input type="hidden"  value="' . $username . '" name="username">';
         echo '</form>';
         echo '<script>document.getElementById("redirectForm").submit();</script>';
+        exit;
 
-    } else if (isset($_POST['logout'])) {
+    } else if (isset($request['logout'])) {
         $params = session_get_cookie_params();
         setcookie(session_name(), '', time() - 86400, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
         session_destroy();
